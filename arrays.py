806effed8618fad62d6614d1d45ea6a71472c377
Aaron Randall <41654628+aaronasherrandall@users.noreply.github.com>
2019-10-17 18:22:02 +0900

    Delete arrays.py

--- arrays.py
+++
@@ -1,8 +0,0 @@
-## Python does not have a native array data sctucture
-## However, it does have the list, which is much more general and can be used as a multidimensional array
-## List in Python is an ordered collection of items which can be of any type
-
-numbers = [10, 20, 30, 40, 50]
-
-# random indexing --> O(1) get items we know the index for
-print(numbers[1])
